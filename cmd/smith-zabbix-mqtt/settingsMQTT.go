d464e7468b7b75daf62d6299d98e1444d78c3934
SmithLEDs <80754288+SmithLEDs@users.noreply.github.com>
2025-10-28 11:54:14 +0400

    change connectRetry

--- cmd/smith-zabbix-mqtt/settingsMQTT.go
+++ cmd/smith-zabbix-mqtt/settingsMQTT.go
@@ -17,7 +17,7 @@ func setupMQTT(cfg *config.Config, log *slog.Logger) *mqtt.ClientOptions {
 		SetWriteTimeout(time.Second).   // Minimal delays on writes
 		SetKeepAlive(10 * time.Second). // Keepalive every 10 seconds so we quickly detect network outages
 		SetPingTimeout(time.Second).    // local broker so response should be quick
-		SetConnectRetry(false).         // Automate connection management (will keep trying to connect and will reconnect if network drops)
+		SetConnectRetry(true).          // Automate connection management (will keep trying to connect and will reconnect if network drops)
 		SetAutoReconnect(true)
 
 	if cfg.Mqtt.Auth {
